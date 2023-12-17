656fa972dca089fd823de169aa5191d97d31e604
GodOfClown2k8 <vonguyenminhkhoi1007@gmail.com>
2023-12-17 11:29:01 +0700

    commit

---
+++ buoi 3/main.js
@@ -0,0 +1,23 @@
+class Human{
+    constructor(nickname,homeland,wide){
+        this.nickname = nickname;
+        this.homeland = homeland;
+        this.wide = wide;
+    }
+}
+
+let Huy = new Human("Change Bell", "Mien Tay","180cm");
+    console.log(Huy);
+class VN extends Human{
+    constructor(nickname,homeland,wide,language){
+        super(nickname,homeland,wide);
+        this.language = language;
+    }
+}
+let huy = new VN ("Huy Nguyen", "Viet Nam","175cm","Vietnamese");
+console.log(huy);
+
+// show(){
+//     console.log("Name": + this.nickname);
+//     return this.nickname
+// }
